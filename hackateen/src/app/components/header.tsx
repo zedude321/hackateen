a9d4ea3db074c1687f9d7eefdd7f4f4c1561a31a
Oxterhal <b.battugs15@gmail.com>
2025-05-03 13:25:03 +0800

    added create-team

--- hackateen/src/app/components/header.tsx
+++ hackateen/src/app/components/header.tsx
@@ -1,6 +1,6 @@
 export const Header = () => {
   return (
-    <div className="fixed right-0 top-0 w-[80vw] h-20 flex justify-between items-center gap-4 p-2 border-b-[1px] opacity-60">
+    <div className="fixed right-0 top-0 w-[80vw] h-20 flex justify-between items-center gap-4 p-2 border-b-[1px] opacity-60 transition-all duration-300">
       <div className="w-1/3 h-full flex justify-center items-center rounded-xl bg-[#1F1F1F]/80">
         Гэрийн даалгавар
       </div>
--- hackateen/src/app/page.tsx
+++ hackateen/src/app/page.tsx
@@ -1,11 +1,21 @@
+"use client";
 import Image from "next/image";
-
+import { useState } from "react";
+import { CreateTeam } from "./components/create-team";
 export default function Home() {
+  const [visible, setVisible] = useState(false);
+
   return (
     <div className="fixed right-0 bottom-0 w-[80vw] p-4  h-[90vh] flex flex-col justify-between items-start">
+      {visible && <CreateTeam setVisible={setVisible} />}
       <p className="text-4xl font-bold block">Hello, Angry Pepe!</p>
       <div className="w-full h-20 flex justify-end items-center">
-        <button className="w-40 h-16 rounded-2xl bg-blue-500">Start</button>
+        <button
+          className="w-40 h-16 rounded-2xl bg-blue-500"
+          onClick={() => setVisible(true)}
+        >
+          Start
+        </button>
       </div>
     </div>
   );
