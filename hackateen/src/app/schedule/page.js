cf99fc6172465ee1053388d09f9184f4bf944591
Oxterhal <b.battugs15@gmail.com>
2025-05-03 13:46:08 +0800

    a

---
+++ hackateen/src/app/schedule/page.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const page = () => {
+  return (
+    <div className="fixed right-0 bottom-0 w-[80vw] p-4  h-[90vh] flex flex-col justify-between items-start">
+      page
+    </div>
+  );
+};
