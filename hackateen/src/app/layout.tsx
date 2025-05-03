95afbd2b8b9a84148ea42b1a06e1003c9caf90e2
Oxterhal <b.battugs15@gmail.com>
2025-05-03 12:05:21 +0800

    a

--- hackateen/src/app/layout.tsx
+++ hackateen/src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import Sidebar from "./components/sidebar";
+import { Header } from "./components/header";
+import Head from "next/head";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -27,6 +29,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Sidebar />
+        {/* <Header /> */}
         {children}
       </body>
     </html>
